fa3a712408e3f2a567671379d24522ff330d9466
deepuezhap <deepuezhap007@gmail.com>
2023-11-15 14:39:05 +0530

    co2

---
+++ s1mca_python/co2/prg1.py
@@ -0,0 +1,6 @@
+n=int(input("Enter the number: "))
+f=1
+for i in range(1,n+1):
+	f=f*i
+	i+=1
+print("Factorial =",f)
---
+++ s1mca_python/co2/prg3.py
@@ -0,0 +1,36 @@
+list1=[]
+n=int(input("enter the no of numberss in first list"))
+print("enter the number")
+for i in range (0,n):
+	a=int(input())
+	list1.append(a)
+
+print(list1)
+sum=0
+for x in list1:
+	sum=sum+x
+
+
+print(sum)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
