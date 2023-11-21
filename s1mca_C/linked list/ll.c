3347413323861ea19ca8f66b3b6f0db836a22efe
deepuezhap <deepuezhap007@gmail.com>
2023-11-21 14:48:59 +0530

    linked list deleted update

--- s1mca_C/linked list/ll.c
+++ s1mca_C/linked list/ll.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -7,65 +7,62 @@ struct node
 	struct node *next;
 };
 
-struct node *head ,*temp;
+struct node *head, *temp,*del;
+
 void display();
 
-struct node* create(int val) //creattion of new node memory is allocated and new node is returned
+struct node *create(int val) // creattion of new node memory is allocated and new node is returned
 {
-	struct node* node=(struct node*)malloc(sizeof(struct node));
-	node->data=val;
-	node->next=NULL;
+	struct node *node = (struct node *)malloc(sizeof(struct node));
+	node->data = val;
+	node->next = NULL;
 	return node;
 }
-void insertion() 
+void insertion()
 {
 	int a;
 	printf("enter the value");
-	scanf("%d",&a);
+	scanf("%d", &a);
 	struct node *newnode = create(a);
-	if(head==NULL)
+	if (head == NULL)
 	{
 
 		head = newnode;
 	}
 	else
 	{
-		temp =head;
-		while(temp->next!=NULL)
+		temp = head;
+		while (temp->next != NULL)
 		{
-			temp=temp->next;
-
+			temp = temp->next;
 		}
-		temp->next=newnode;
-		
+		temp->next = newnode;
 	}
 }
 
 void insertat()
 {
-	int pos,a ;
+	int pos, a;
 	printf("enter the position to insert  and the value: ");
-	scanf("%d %d",&pos,&a);
+	scanf("%d %d", &pos, &a);
 	struct node *newnode = create(a);
-	int i =1;
-	temp=head;
-	if(pos==1)
+	int i = 1;
+	temp = head;
+	if (pos == 1)
 	{
-		newnode->next=head;
+		newnode->next = head;
 		head = newnode;
-
 	}
 	else
 	{
 
-
-	while(i<pos-1)
-	{
-		temp=temp->next;
-		i++;
-	}
-	newnode->next=temp->next;
-	temp->next = newnode;
+		while (i < pos - 1)
+		{
+			temp = temp->next;
+			i++;
+		}
+		newnode->next = temp->next;
+		temp->next = newnode;
 	}
 	display();
 }
@@ -74,56 +71,78 @@ void display()
 {
 	temp = head;
 	printf("Linked list is : ");
-	while(temp!=NULL) 
-	{	
-		printf("%d  ",temp->data);
-		temp=temp->next;
+	while (temp != NULL)
+	{
+		printf("%d  ", temp->data);
+		temp = temp->next;
 	}
 }
 
 
-struct node* del;
+
 void deletebeg()
 {
-	del= head;
-	printf("%d ",del->data);
+	del = head;
+	head = head->next;
+	printf("%d is deleted \n", del->data);
 	free(del);
-
-
+	display();
 }
 
+void deleteend()
+{
+	
+	temp = head;
+	while (temp->next!= NULL)
+	{
+		
+		temp = temp->next;
+	}
+	printf("%d is to be deleted", temp->data);
+	del=temp;
+	free(del);
 
+}
 
 void main()
 {
-int n;
-printf("1.Creation \n2.Display\n3.Insertion at a particular position\n4.exit \n");
+	int n;
+	printf("1.Creation \n2.Display\n3.Insertion at a particular position\n4.delete at beg \n5.delete at particular pos\n6.delete at end \n7.exit \n");
 
-while(1)
-{
-	printf("Enter your choice : ");
-	scanf("%d",&n);
-	switch(n)
+	while (1)
 	{
-		case 1 : insertion();
+		printf("\n Enter your choice : ");
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 1:
+			insertion();
 			break;
 
-		case 2 : display();
-			break;
-		
-		case 3 : insertat();
+		case 2:
+			display();
 			break;
 
-		case 4 : deletebeg();
+		case 3:
+			insertat();
 			break;
 
-		case 5 :exit(0);
+		case 4:
+			deletebeg();
+			break;
 
-		default : printf("enter a valid choice!!");
-		break;
+		case 5:
+			deleteend();
+			// printf("Hello");
+			break;
+
+		case 6:
+			exit(0);
+			break;
+
+		default:
+			printf("enter a valid choice!!");
+			break;
+		}
 	}
-
-}
-
-
 }
