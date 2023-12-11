de8bac2dcf32521752b3ffac4bd197b3d3e361c5
deepuezhap <deepuezhap007@gmail.com>
2023-12-11 15:39:58 +0530

    book

--- s1mca_python/co4/book.py
+++ s1mca_python/co4/book.py
@@ -4,18 +4,19 @@ class Publisher:
 
 class Book(Publisher):
     def __init__(self,title,author,pub_name):
-        super().__init__()
+        super().__init__(pub_name)
         self.title=title
         self.author=author
-        self.pub_name=pub_name
+        
+class Python(Book):
+    def __init__(self,price,nop,title,author,pub_name):
+        super().__init__(title,author,pub_name)
+        self.price=price
+        self.nop=nop
 
     def __str__(self):
-        return f"{self.title} and {self.author} and {self.pub_name}"
+        return f"{self.title} and {self.author} and {self.pub_name} price:{self.price} and Pages : {self.nop}"
 
 
-
-b= Book("maths","raman","KK books")
-b2=Book("kak","kol")
-
+b= Python(1500,200,"Harry potter"," jk rowling","KK books")
 print(b)
-print(b2)
